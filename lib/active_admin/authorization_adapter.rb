c25d59b32e6c8a028ddf68c5aeb6484f1763271b
timwis <tim@timwis.com>
2020-11-10 08:57:07 +0000

    Allow authorization adapters to differentiate between new/create and edit/update, and differentiate in pundit adapter Fixes activeadmin/activeadmin#6529

--- lib/active_admin/authorization_adapter.rb
+++ lib/active_admin/authorization_adapter.rb
@@ -3,7 +3,9 @@ module ActiveAdmin
   # Default Authorization permissions for Active Admin
   module Authorization
     READ = :read
+    NEW = :create
     CREATE = :create
+    EDIT = :update
     UPDATE = :update
     DESTROY = :destroy
   end

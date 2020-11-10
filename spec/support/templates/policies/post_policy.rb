ec94be1bcf12cdd0482060126b2e099677a914db
timwis <tim@timwis.com>
2020-11-10 08:58:09 +0000

    Add tests for differentiating new/create in authorization adapter

--- spec/support/templates/policies/post_policy.rb
+++ spec/support/templates/policies/post_policy.rb
@@ -1,4 +1,12 @@
 class PostPolicy < ApplicationPolicy
+  def new?
+    true
+  end
+
+  def create?
+    record.category.name != "Announcements" || user.admin
+  end
+
   def update?
     record.author == user
   end
